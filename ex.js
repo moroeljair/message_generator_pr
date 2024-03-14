0a841cd8c7d4fd109078eddfc141c935793a2e06
Jair Morocho <jairmorocho99@gmail.com>
2024-03-14 16:33:58 -0500

    Listas las palabras

---
+++ ex.js
@@ -0,0 +1,22 @@
+const randomNum = num =>{
+    return Math.floor(Math.random() * num);
+}
+
+const temas = [
+    ["Perro",
+    "Gato",
+    "León",
+    "Elefante",
+    "Tigre"],
+    ["Fútbol",
+    "Baloncesto",
+    "Tenis",
+    "Natación",
+    "Atletismo"],
+    ["La vida es bella.",
+    "El tiempo es oro.",
+    "La práctica hace al maestro.",
+    "El que no arriesga, no gana.",
+    "La perseverancia es la clave del éxito."]
+]
+
